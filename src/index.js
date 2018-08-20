1b614b7b3ee143b90bc85fe0c3ce5884b7cad39b
Josh <joshua.ross.miller@gmail.com>
2018-08-20 10:04:33 -0400

    1st

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
 
-  const imageId = 1 //Enter your assigned imageId here
+  const imageId = 2 //Enter your assigned imageId here
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,6 +9,32 @@ document.addEventListener('DOMContentLoaded', function() {
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
 
-
+  getPicture(imageURL,imageId)
 })
 
+
+function getPicture(imageURL,imageId) {
+  fetch(imageURL)
+  .then(res=>res.json())
+  .then(json=> renderImage(json))
+}
+
+function renderImage(json) {
+  console.log(json)
+  let img = document.querySelector('#image')
+  let h4 = document.querySelector('#name')
+  let span = document.querySelector('#likes')
+  let ul = document.querySelector('#comments')
+
+  img.src = json.url
+  h4.innerHTML = json.name
+  span.innerHTML = json.like_count
+
+  json.comments.forEach(comment=>{
+    let li = document.createElement('li')
+    li.id = `comment-${comment.id}`
+    li.innerHTML = comment.content
+    ul.append(li)
+  })
+
+}
